f0be310352e83b315ebf358a2b10ffb1e0174db9
Jayhmz <Jayhmz10@gmail.com>
2023-01-22 21:47:57 +0100

    Update model dto

--- src/main/java/com/plantacion/inventory/app/model/dto/AssetComponentDTO.java
+++ src/main/java/com/plantacion/inventory/app/model/dto/AssetComponentDTO.java
@@ -1,11 +1,52 @@
 package com.plantacion.inventory.app.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 public class AssetComponentDTO {
+    @JsonProperty("component_id")
+    @Size(min = 12, max = 12, message = "Confirm the component id")
     private String component_Id;
+
+    @JsonProperty("component_name")
+    @NotNull(message = "component name cannot be empty")
+    @Size(min = 3, message = "Invalid component name")
     private String component_Name;
+
+    @JsonProperty("component_category")
+    @Size(min = 1, message = "Invalid category name")
     private String component_Category;
-    private String new_ComponentId;
+
+    @JsonProperty("component_location")
+    @NotNull(message = "Specify the location of the component")
+    private String component_Location;
+
+    @JsonProperty("component_user")
+    @Size(min = 5, message = "Invalid component user/office")
+    @NotNull(message = "component user/office cannot be empty")
+    private String component_User;
+
+    @JsonProperty("serial_no")
+    private String component_Serial_No;
+
+    @JsonProperty("new_component_id")
+    @Size(min = 1, max = 12, message = "Ensure to input valid QR code")
+    @NotNull(message = "Ensure to input the new id on the QR code")
+    private String new_componentId;
+
+    @JsonProperty("status")
+    @NotNull(message = "What is the status of the component?")
+    private String status;
+
+    @JsonProperty("movement_details")
+    private String movement_Details;
+
+    @JsonProperty("availability_details")
+    @NotNull(message = "What is the availability state of the component?")
+    private String availability_Details;
 }
--- src/main/java/com/plantacion/inventory/app/model/dto/AssetDTO.java
+++ src/main/java/com/plantacion/inventory/app/model/dto/AssetDTO.java
@@ -1,15 +1,18 @@
 package com.plantacion.inventory.app.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.plantacion.inventory.app.model.domain.AssetComponent;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 public class AssetDTO {
 
     @JsonProperty("asset_id")
-    @Size(min = 11, max = 11, message = "maximum length exceeded")
+    @Size(min = 12, max = 12, message = "Confirm the asset id")
     private String asset_Id;
 
     @JsonProperty("asset_name")
@@ -21,15 +24,33 @@ public class AssetDTO {
     @Size(min = 1, message = "Invalid category name")
     private String asset_Category;
 
+    @JsonProperty("asset_location")
+    @NotNull(message = "Specify the location of the asset")
+    private String asset_Location;
+
     @JsonProperty("asset_user")
-    @Size(min = 1, message = "Invalid asset user/office")
+    @Size(min = 5, message = "Invalid asset user/office")
     @NotNull(message = "Asset user/office cannot be empty")
     private String asset_User;
+
+    @JsonProperty("serial_no")
+    private String asset_Serial_No;
 
     @JsonProperty("new_asset_id")
     @Size(min = 1, max = 12, message = "Ensure to input valid QR code")
     @NotNull(message = "Ensure to input the new id on the QR code")
     private String new_AssetId;
 
+    @JsonProperty("status")
+    @NotNull(message = "What is the status of the asset?")
+    private String status;
 
+    @JsonProperty("movement_details")
+    private String movement_Details;
+
+    @JsonProperty("availability_details")
+    @NotNull(message = "What is the availability state of the asset?")
+    private String availability_Details;
+
+    private List<AssetComponent> components;
 }
