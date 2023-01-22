a49d196bbf04813a64b7cfc2628af20354785e30
Jayhmz <Jayhmz10@gmail.com>
2023-01-22 21:14:01 +0100

    Update Asset.java

--- src/main/java/com/plantacion/inventory/app/model/domain/Asset.java
+++ src/main/java/com/plantacion/inventory/app/model/domain/Asset.java
@@ -10,20 +10,43 @@ import lombok.NoArgsConstructor;
 import java.util.List;
 
 @Entity
+@Table(name = "assets")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class Asset {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
+
+    @Column(name = "asset_id", nullable = true, unique = true)
     private String asset_Id;
+
+    @Column(name = "asset_name", nullable = false)
     private String asset_Name;
+
+    @Column(name = "asset_category", nullable = false)
     private String asset_Category;
+
+    @Column(name = "asset_location", nullable = false)
+    private String asset_Location;
+
+    @Column(name = "asset_user", nullable = false)
     private String asset_User;
+
+    @Column(name = "asset_serial_no", nullable = true, unique = true)
+    private String asset_Serial_No;
+
+    @Column(name = "new_asset_id", nullable = false, unique = true)
     private String new_AssetId;
 
-    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    private List<AssetComponent> component;
+    @Column(name = "status", nullable = false)
+    private String status;
+    @Column(name = "movement_details", nullable = true)
+    private String movement_Details;
+    @Column(name = "availability_details", nullable = false)
+    private String availability_Details;
+
 }
