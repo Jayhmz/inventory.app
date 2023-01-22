cc37bf4e397ff9b674c57116db2fb44e44d3fa30
Jayhmz <Jayhmz10@gmail.com>
2023-01-22 14:47:22 +0100

    Create AssetComponentDTO

---
+++ src/main/java/com/plantacion/inventory/app/model/dto/AssetComponentDTO.java
@@ -0,0 +1,11 @@
+package com.plantacion.inventory.app.model.dto;
+
+import lombok.Data;
+
+@Data
+public class AssetComponentDTO {
+    private String component_Id;
+    private String component_Name;
+    private String component_Category;
+    private String new_ComponentId;
+}
